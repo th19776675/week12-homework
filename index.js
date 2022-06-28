bf796e4fd3c87efdbe5170ab225226da06ca50ab
Tina Selly <tina@Tinas-MacBook-Pro.local>
2022-06-28 14:41:18 +0800

    Database Schema Added

---
+++ index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
