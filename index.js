af2838cb445fb8f328f2e44c17a873d8e4c93658
Tina Selly <tina@Tinas-MacBook-Pro.local>
2022-06-28 17:03:39 +0800

    Initial prompting functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const inquirer = require("inquirer");
 require('dotenv').config();
 
 // Connect to database
@@ -8,7 +9,54 @@ const db = mysql.createConnection(
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-  },
-  console.log(`Connected to the employees_db database.`)
+  }
 );
 
+function init() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "menu",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+      default: "View All Employees"
+    }
+  ]).then((response) => {
+    switch (response.menu) {
+      case "View All Employees":
+        view(employee);
+      break;
+      case "Add Employee":
+        add(employee);
+      break;
+      case "Update Employee Role":
+        updateEmployeeRole()
+      break;
+      case "View All Roles":
+        view(role);
+      break;
+      case "Add Role":
+        add(role);
+      break;
+      case "View All Departments":
+        view(department);
+      break;
+      case "Add Department":
+        add(department);
+      break;
+      case "Quit":
+      process.exit();
+    }
+  })
+}
+
+init();
